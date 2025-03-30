3071da8847b33bc24fc8604f2d8c5a30c6eb5b97
Pranav Pundeer <PPundeer@slb.com>
2025-03-30 22:18:10 +0530

    implemented client-server chat over TCP

---
+++ client-server-comm/client.cpp
@@ -0,0 +1,69 @@
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+#include <stdio.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
+
+using namespace std;
+
+#define SERVER_IP "127.0.0.3"
+#define PORT 8100
+
+int main(int argc, char* argv[]) {
+    int client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if(client_socket < 0) {
+        fprintf(stderr, "socket() failed\n");
+        exit(1);
+    }
+
+    struct sockaddr_in client_addr = {};
+    client_addr.sin_family = AF_INET;
+    client_addr.sin_port = htons(PORT);
+    socklen_t addr_len = sizeof(client_addr);
+    
+    inet_pton(AF_INET, SERVER_IP, &(client_addr.sin_addr.s_addr));
+
+    int rv = connect(client_socket, (const struct sockaddr *)&client_addr, addr_len);
+    if(rv) {
+        fprintf(stderr, "connect() failed\n");
+        exit(1);
+    } else {
+        printf("[+] Connected to server\n");
+    }
+
+    while(true) {
+        string msg;
+        getline(cin, msg);
+        if (msg.empty()) continue;
+    
+        ssize_t bytes_written = write(client_socket, msg.c_str(), msg.size());
+
+        if (bytes_written < 0)
+            perror("write() failed"); 
+    
+        if (msg == "exit") break;
+
+        char buffer[256] = {0}; 
+        ssize_t n = read(client_socket, buffer, sizeof(buffer) - 1);
+        
+        if(n <= 0) {
+            if (n == 0) {
+                printf("Server closed the connection.\n");
+            } else {
+                perror("read() failed");
+            }
+            break;
+        }
+    
+        buffer[n] = '\0';
+        printf("Server: %s\nYou: ", buffer);
+        fflush(stdout);
+    
+    }
+    printf("[-] Closing connection from client side.\n");
+    close(client_socket);
+    return 0;
+}
--- client-server-comm/server.cpp
+++ client-server-comm/server.cpp
@@ -1,12 +1,8 @@
-#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
-#include <errno.h>
 #include <iostream>
-#include <ctime>
+#include <stdio.h>
 #include <unistd.h>
-#include <thread>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/ip.h>
@@ -14,14 +10,9 @@
 using namespace std;
 
 #define SERVER_IP "127.0.0.3"
+#define PORT 8100
 
 int main(int argc, char* argv[]){
-    if(argc < 2) {
-        fprintf(stderr, "Port number not provided. Provide a port number to bind to.\n");
-        exit(1);
-    }
-
-    int port = atoi(argv[1]);
     int initial_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (initial_socket < 0) {
         fprintf(stderr, "socket() failed\n");
@@ -30,7 +21,7 @@ int main(int argc, char* argv[]){
 
     struct sockaddr_in addr = {};
     addr.sin_family = AF_INET;
-    addr.sin_port = htons(port);
+    addr.sin_port = htons(PORT);
     inet_pton(AF_INET, SERVER_IP, &(addr.sin_addr.s_addr));
     socklen_t addr_len = sizeof(addr);
 
@@ -39,7 +30,7 @@ int main(int argc, char* argv[]){
         fprintf(stderr, "bind() failed\n");
         exit(1);
     } else {
-        printf("[+] binded to port:%d\n", port);
+        printf("[+] binded to port:%d\n", PORT);
     }
 
     rv = listen(initial_socket, SOMAXCONN);
@@ -50,45 +41,43 @@ int main(int argc, char* argv[]){
         printf("[+] server is listening...\n");
     }
 
-    while(true) {
-        struct sockaddr_in client_addr = {};
-        socklen_t socklen = sizeof(client_addr);
+    struct sockaddr_in client_addr = {};
+    socklen_t socklen = sizeof(client_addr);
 
-        int client_sock = accept(initial_socket, (struct sockaddr*)&client_addr, &socklen);
-        if (client_sock < 0) {
-            fprintf(stderr, "[-] Connection for client with socket %d failed.\n", client_sock);
-        } else {
-            printf("[+] 1 new connection added: %d\n", client_sock);
-        }
-
-        while(true) {
-            char buffer[256];
-            int n = read(client_sock, buffer, sizeof(buffer) - 1);
-            if(n<0) {
-                fprintf(stderr, "read() failed\n");
-            }
-            
-            printf("Client: %s\nYou: ", buffer);
-            fflush(stdout);
-            
-            string msg;
-            getline(cin, msg);
-            memcpy(buffer, msg.c_str(), msg.size());
-            buffer[msg.length()] = '\0';
-
-            n = write(client_sock, buffer, strlen(buffer));
-            if(n<0) {
-                fprintf(stderr, "write() failed\n");
-            }
-
-            int close_conn = strcmp("exit", buffer);
-
-            if(close_conn == 0) break;
-        }
-        printf("[-] connection terminated with client %d\n", client_sock);
-        close(client_sock);
+    cout << "waiting on accept syscall";
+    int client_sock = accept(initial_socket, (struct sockaddr*)&client_addr, &socklen);
+    if (client_sock < 0) {
+        fprintf(stderr, "[-] Connection for client with socket %d failed.\n", client_sock);
+    } else {
+        printf("[+] 1 new connection added: %d\n", client_sock);
     }
-    printf("[-] server shutting down...\n");
+    cout << "after accept";
+
+    while(true) {
+        char buffer[256] = {0};
+        ssize_t n = read(client_sock, buffer, sizeof(buffer) - 1);
+        if (n < 0) {
+            perror("read() failed");
+        } else if (n == 0) {
+            cout << "Client disconnected\n";
+            break;
+        } 
+
+        printf("Client: %s\nYou: ", buffer);
+        fflush(stdout);
+        
+        string msg;
+        getline(cin,msg);
+        if (msg.empty()) continue; // Avoid sending empty messages
+    
+        write(client_sock, msg.c_str(), msg.size());
+        
+        if (msg == "exit") break;
+    }
+    
+    printf("[-] connection terminated with client %d\n", client_sock);
+    close(client_sock);
     close(initial_socket);
+    printf("[-] server shutting down...\n");
     return 0;
 }
